07f2ac830eb4fe0d114fd743aa31d31f7c09353a
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2020-06-11 17:34:00 +0530

    demonstration of comment handling functionality

---
+++ cplusplus/helloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+int main(void){
+// This is a working model for converting python scripts to .cpp files
+cout <<  "\nHello World!" ;  // comment 1
+cout <<  "\nThis is Bhasha" ;  // comment 2
+return 0; 
+}
--- cplusplus/temp.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-using namespace std;
-int main(void){
-cout << "Hello" ; cout << endl;
-cout << "World" ; cout << endl;
-return 0; 
-}
